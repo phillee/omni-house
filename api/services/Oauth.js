1abff0f1e7131a78794c1defb0f6bbacdaf89304
Philip Lee <philip.a.lee@gmail.com>
2017-03-25 21:46:44 -0700

    return if no client

--- api/services/Oauth.js
+++ api/services/Oauth.js
@@ -94,6 +94,8 @@ server.exchange(oauth2orize.exchange.code(function(client, code, redirectURI, do
 server.exchange(oauth2orize.exchange.refreshToken((client, refreshToken, scope, done) => {
   console.log('exchanging, client is', client);
 
+  if (!client) reutrn done(null);
+
   RefreshToken.findOne({ token: refreshToken }, (err, token) => {
     if (err) return done(err);
     if (!token) return done(null, false);
